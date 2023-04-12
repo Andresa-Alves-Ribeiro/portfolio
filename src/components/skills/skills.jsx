8f523a9ae2196d60dd8b8e617671898f5687839c
Andresa Alves <andresa_15ga@hotmail.com>
2023-04-11 21:42:55 -0300

    terminando estilização das skills

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -20,23 +20,56 @@ const Skills = () => {
     return (
         <div className="stack-list">
             <h1 className="glitch2" data-text="My Skills">My Skills</h1>
+            <section className="wave-top"></section>
             <div className='stacks-logo'>
-                <img src={ReactLogo} alt="ReactJS Logo" className='logo' />
-                <img src={Typescript} alt="Typescript Logo" className='logo'/>
-                <img src={Javascript} alt="Javascript Logo" className='logo'/>
-                <img src={Vite} alt="Vite Logo" className='logo'/>
-                <img src={Next} alt="Next Logo" className='logo'/>
-                <img src={Sass} alt="Sass Logo" className='logo'/>
-                <img src={Tailwind} alt="Tailwind Logo" className='logo'/>
-                <img src={Bootstrap} alt="Bootstrap Logo" className='logo'/>
-                <img src={Figma} alt="Figma Logo" className='logo'/>
-                <img src={StyledComponents} alt="Styled Components Logo" className='logo'/>
-                <img src={Postman} alt="Postman Logo" className='logo'/>
-                <img src={Node} alt="NodeJS Logo" className='logo'/>
-                <img src={Mysql} alt="MySQL Logo" className='logo'/>
-                <img src={Git} alt="Git Logo" className='logo'/>
-                <img src={VSCode} alt="VSCode Logo" className='logo'/>
+                <a href="https://www.reactjs.org" target="_blank" rel="noreferrer">
+                    <img src={ReactLogo} alt="ReactJS Logo" className='logo' />
+                </a>
+                <a href="https://www.typescriptlang.org" target="_blank" rel="noreferrer">
+                    <img src={Typescript} alt="Typescript Logo" className='logo' />
+                </a>
+                <a href="https://www.javascript.com" target="_blank" rel="noreferrer">
+                    <img src={Javascript} alt="Javascript Logo" className='logo' />
+                </a>
+                <a href="https://www.vitejs.dev" target="_blank" rel="noreferrer">
+                    <img src={Vite} alt="Vite Logo" className='logo' />
+                </a>
+                <a href="https://nextjs.org" target="_blank" rel="noreferrer">
+                    <img src={Next} alt="Next Logo" className='logo' />
+                </a>
+                <a href="https://sass-lang.com" target="_blank" rel="noreferrer">
+                    <img src={Sass} alt="Sass Logo" className='logo' />
+                </a>
+                <a href="https://tailwindcss.com" target="_blank" rel="noreferrer">
+                    <img src={Tailwind} alt="Tailwind Logo" className='logo' />
+                </a>
+                <a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
+                    <img src={Bootstrap} alt="Bootstrap Logo" className='logo' />
+                </a>
+                <a href="https://www.figma.com" target="_blank" rel="noreferrer">
+                    <img src={Figma} alt="Figma Logo" className='logo' />
+                </a>
+                <a href="https://styled-components.com" target="_blank" rel="noreferrer">
+                    <img src={StyledComponents} alt="Styled Components Logo" className='logo' />
+                </a>
+                <a href="https://www.postman.com" target="_blank" rel="noreferrer">
+                    <img src={Postman} alt="Postman Logo" className='logo' />
+                </a>
+                <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+                    <img src={Node} alt="NodeJS Logo" className='logo' />
+                </a>
+                <a href="https://www.mysql.com" target="_blank" rel="noreferrer">
+                    <img src={Mysql} alt="MySQL Logo" className='logo' />
+                </a>
+                <a href="https://git-scm.com" target="_blank" rel="noreferrer">
+                    <img src={Git} alt="Git Logo" className='logo' />
+                </a>
+                <a href="https://code.visualstudio.com" target="_blank" rel="noreferrer">
+                    <img src={VSCode} alt="VSCode Logo" className='logo' />
+                </a>
             </div>
+
+            <section className="wave-bottom"></section>
         </div>
     );
 };
