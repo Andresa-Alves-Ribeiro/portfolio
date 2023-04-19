3a7d4bec6253372ed22a7183d1e5d65b231855a6
Andresa Alves <andresa_15ga@hotmail.com>
2023-04-18 21:45:59 -0300

    estilização contact finalizada

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,65 +1,67 @@
-import React from 'react';
-import './contact.css';
-import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
+import axios from 'axios';
+import './contact.scss';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const [formStatus, setFormStatus] = useState('');
+
   const onSubmit = async (data) => {
     try {
-      const response = await fetch('/enviar-email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-
-      if (response.ok) {
-        console.log('Email enviado com sucesso!');
-      } else {
-        console.error('Erro ao enviar o email.');
-      }
+      await axios.post('http://localhost:3001/enviar-email', data);
+      setFormStatus('success');
     } catch (error) {
-      console.error('Erro ao enviar o email:', error);
+      console.error(error);
+      setFormStatus('error');
     }
   };
 
   return (
-    <div>
-      <h2>Fale Comigo</h2>
+    <div className="contact-form">
+      <div id="wrapper">
+        <h1 className="glitch-contact" data-text="Fale Comigo">Fale Comigo</h1>
+      </div>
       <p>Resta alguma dúvida? Preencha os campos abaixo com os seguintes dados e entre em contato comigo.</p>
 
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label>
-            Nome:
-            <input {...register("nome", { required: true })} />
-            {errors.nome && <span>Este campo é obrigatório</span>}
-          </label>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <label class="form__label">
+          <input className="form__input" type="text" {...register('nome', { required: true })} placeholder='Seu nome aqui' />
+          <span className="form__error">{errors.nome && 'Este campo é obrigatório'}</span>
+        </label>
 
-          <label>
-            Email:
-            <input {...register("email", { required: true })} />
-            {errors.email && <span>Este campo é obrigatório</span>}
-          </label>
+        <label className="form__label">
+          <input className="form__input" type="email" {...register('email', { required: true })} placeholder='email@exemplo.com.br' />
+          <span className="form__error">{errors.email && 'Este campo é obrigatório'}</span>
+        </label>
 
-          <label>
-            Assunto:
-            <input {...register("assunto", { required: true })} />
-            {errors.assunto && <span>Este campo é obrigatório</span>}
-          </label>
+        <label className="form__label">
+          <input className="form__input" type="text" {...register('assunto', { required: true })} placeholder='Assunto' />
+          <span className="form__error">{errors.assunto && 'Este campo é obrigatório'}</span>
+        </label>
 
-          <label>
-            Mensagem:
-            <textarea {...register("mensagem", { required: true })} />
-            {errors.mensagem && <span>Este campo é obrigatório</span>}
-          </label>
+        <label className="form__label">
+          <textarea className="form__textarea" {...register('mensagem', { required: true })} placeholder='Escreva sua mensagem aqui' />
+          <span className="form__error">{errors.mensagem && 'Este campo é obrigatório'}</span>
+        </label>
 
-          <button type="submit">Enviar</button>
-        </form>
-      </div>
+        <button className="form__submit" type="submit">
+          <span>Enviar</span>
+          <div className="form__submit__circle"></div>
+        </button>
+
+        {formStatus === 'success' && (
+          <p className="form__status form__status--success">Email enviado com sucesso!</p>
+        )}
+
+        {formStatus === 'error' && (
+          <p className="form__status form__status--error">Erro ao enviar o email.</p>
+        )}
+      </form>
     </div>
-  )
-}
+
+
+  );
+};
 
 export default Contact;
