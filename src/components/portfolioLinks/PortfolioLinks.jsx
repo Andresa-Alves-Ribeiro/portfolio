7c4d52a757442c179b04eda967ec00e3b0cddd61
Andresa Alves <andresa_15ga@hotmail.com>
2023-04-25 21:41:30 -0300

    colocando mais projetos

--- src/components/portfolioLinks/PortfolioLinks.jsx
+++ src/components/portfolioLinks/PortfolioLinks.jsx
@@ -18,13 +18,19 @@ export const portfolioLinks = [
         stacks: ['REACTJS', 'CSS', 'AXIOS'],
     },
     {
-        title: 'Recriando ChatGPT',
-        image: 'https://user-images.githubusercontent.com/94997593/227315882-be76973e-0e0f-48d0-9a4d-382bd8cf133c.png',
-        stacks: ['REACTJS', 'CSS', 'AXIOS'],
+        title: 'Recriando Instagram',
+        image: 'https://user-images.githubusercontent.com/94997593/234426187-778a36e3-5d86-4684-b63a-d21e59235cdc.png',
+        stacks: ['HTML', 'CSS'],
+        description: 'Recriação da página de login do Instagram.',
+        liveDemoUrl: 'https://recriando-instagram-eight.vercel.app/',
+        repoUrl: 'https://github.com/Andresa-Alves-Ribeiro/Recriando-instagram'
     },
     {
         title: 'Flex Turismo',
         image: 'https://user-images.githubusercontent.com/94997593/231608000-c6aa2406-8fad-4a3c-9a5a-aa776eeff32a.png',
         stacks: ['HTML', 'CSS'],
+        description: 'Projeto de um website de uma empresa de viagens.',
+        liveDemoUrl: 'https://flex-turismo-seven.vercel.app/',
+        repoUrl: 'https://github.com/Andresa-Alves-Ribeiro/Flex-Turismo'
     }
 ];
