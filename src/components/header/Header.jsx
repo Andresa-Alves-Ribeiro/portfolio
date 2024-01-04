0a51a2d5720e74683c683823fdd4ba403dbf3223
Andresa <andresa_15ga@hotmail.com>
2024-01-04 20:52:43 -0300

    atualizando subtitle do header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className='title_loop'>
           <h5 className='title-primary'>Olá!! Eu sou</h5>
           <div className='title'><span>Andresa</span> Alves</div>
-          <h5 className='subtitle'>Desenvolvedora Full-Stack</h5>
+          <h5 className='subtitle'>Desenvolvedora Front-End</h5>
         </div>
     </header>
   )
