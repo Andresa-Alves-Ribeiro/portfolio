0affe219ffce851b9c7c6cb604fd6fd7efc51900
Andresa <andresa_15ga@hotmail.com>
2025-03-31 21:23:57 -0300

    style: mudando layout do header e footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,63 +1,56 @@
 import React from 'react';
+import { ReactComponent as LinkedinIcon } from '../../assets/linkedin.svg';
+import { ReactComponent as GithubIcon } from '../../assets/github-nav.svg';
+import { ReactComponent as WhatsappIcon } from '../../assets/whatsapp.svg';
 import './footer.scss';
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="column-contact">
-            <h4 className="footer__title">Menu</h4>
+    return (
+        <footer className="footer">
+            <div className="footer__content">
+                <div className="column-contact">
+                    <h3 className="footer__title">Contato</h3>
+                    <div className="footer__contact-info">
+                        <p className="footer__text">
+                            <strong>Email:</strong> andresa_15ga@hotmail.com
+                        </p>
+                        <p className="footer__text">
+                            <strong>Localização:</strong> Cosmópolis, SP
+                        </p>
+                    </div>
+                </div>
 
-            <ul className="footer__list">
-              <li>
-                <a href="#presentation" className="footer__item link">Home</a>
-              </li>
-              <li>
-                <a href="#about" className="footer__item link">Sobre Mim</a>
-              </li>
-              <li>
-                <a href="#skills" className="footer__item link">Skills</a>
-              </li>
-              <li>
-                <a href="#projects" className="footer__item link">Portfólio</a>
-              </li>
-              <li>
-                <a href="#contact" className="footer__item link">Contato</a>
-              </li>
-            </ul>
-          </div>
+                <div className="column-contact">
+                    <h3 className="footer__title">Links</h3>
+                    <ul className="footer__list">
+                        <li><a href="#about" className="footer__item">Sobre</a></li>
+                        <li><a href="#skills" className="footer__item">Skills</a></li>
+                        <li><a href="#projects" className="footer__item">Projetos</a></li>
+                        <li><a href="#contact" className="footer__item">Contato</a></li>
+                    </ul>
+                </div>
 
-          <div className="column-contact">
-            <h4 className="footer__title">Redes Sociais</h4>
+                <div className="column-contact">
+                    <h3 className="footer__title">Redes Sociais</h3>
+                    <div className="footer__social">
+                        <a href="https://www.linkedin.com/in/andresa-alves-ribeiro/" target="_blank" rel="noopener noreferrer" className="link">
+                            <LinkedinIcon className="footer__icon" />
+                        </a>
+                        <a href="https://github.com/Andresa-Alves-Ribeiro" target="_blank" rel="noopener noreferrer" className="link">
+                            <GithubIcon className="footer__icon" />
+                        </a>
+                        <a href="https://api.whatsapp.com/send?phone=5519997516202" target="_blank" rel="noopener noreferrer" className="link">
+                            <WhatsappIcon className="footer__icon" />
+                        </a>
+                    </div>
+                </div>
+            </div>
 
-            <ul className="footer__list">
-              <li>
-                <a href="https://www.linkedin.com/in/andresa-alves-ribeiro/" target="_blank" rel="noopener noreferrer" className="footer__item link">Linkedin</a>
-              </li>
-              <li>
-                <a href="https://github.com/Andresa-Alves-Ribeiro" target="_blank" rel="noopener noreferrer" className="footer__item link">Github</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="column-contact">
-            <h4 className="footer__title">Contato</h4>
-            
-            <ul className="footer__list">
-              <li className="footer__item">(19) 99751-6202</li>
-              <li className="footer__item">andresa_15ga@hotmail.com</li>
-              <li className="footer__item">Cosmópolis - SP</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div className="footer__bottom">
-        <p>Copyright &copy; 2025 Andresa Alves Ribeiro</p>
-      </div>
-    </footer>
-  );
+            <div className="footer__bottom">
+                <p>&copy; {new Date().getFullYear()} Andresa Alves. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
